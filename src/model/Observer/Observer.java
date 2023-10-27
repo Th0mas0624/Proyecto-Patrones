59e503c002cb5b9688445df6f82f0bd02088559e
Carlos Bermudez <106134881+carlosbermudezgalvan@users.noreply.github.com>
2023-10-27 06:05:35 -0500

    Delete src directory

--- src/model/Observer/Observer.java
+++
@@ -1,9 +0,0 @@
-package model.Observer;
-
-import model.Decorator.Player;
-
-public interface Observer {
-    void updatePlayer(Player playerModel);
-    void updatePlayerHealth(float health);
-    
-}
